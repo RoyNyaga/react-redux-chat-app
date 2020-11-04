bee1b8a7b3df7c5e30d2a8b3abb7a176419beba6
RoyNyaga <nyagaandreroy@gmail.com>
2020-11-04 16:22:52 +0100

    add forms

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,12 +1,43 @@
 import Layout from "../../components/Layout";
 import Card from "../../components/UI/Card";
+import './style.css';
+import React, { useState, useEffect } from 'react'
+
+
 
 const LoginPage = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <Layout>
-      <div>
+      <div className="loginContainer">
         <Card>
-          <form></form>
+          <form>
+            
+            <input 
+              name="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+
+            <input 
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+
+
+            <div>
+              <button>Login</button>
+            </div>
+
+          </form>
         </Card>
       </div>
     </Layout>
--- src/containers/RegisterPage/index.js
+++ src/containers/RegisterPage/index.js
@@ -1,11 +1,66 @@
 import Layout from "../../components/Layout";
+import Card from '../../components/UI/Card';
+import React, { useState } from 'react';
+
 
 const RegisterPage = () => {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <Layout>
-      <div>Register Page</div>
+      <div className="registerContainer">
+        <Card>
+          <form>
+
+            <h3>Sign up</h3>
+
+          <input 
+              name="firstName"
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder="First Name"
+            />
+
+            <input 
+              name="lastName"
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder="Last Name"
+            />
+
+            <input 
+              name="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+
+            <input 
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+
+            <div>
+              <button>Sign up</button>
+            </div>
+
+
+
+          </form>
+        </Card>
+      </div>
     </Layout>
-    );
+  )
 }
  
 export default RegisterPage 
